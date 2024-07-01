7d038cdefc0bfd4ded94e17adc5f05e251c6e163
pssvlng <info@passivlingo.com>
2024-07-01 17:23:57 +0200

    add some queries

--- queries.py
+++ queries.py
@@ -14,3 +14,28 @@ select distinct * where {
 FILTER(CONTAINS(LCASE(?label), "deutsch"))
 }
 """
+
+'''
+Suche nach allen Unterelementen, die mit einem bestimmten Lehrplanelement verbunden sind
+<https://w3id.org/lehrplan/ontology/LP_00000008>: hat_teil
+<https://lehrplan.yovisto.com/resource/lp/rp/c688300f-1c49-11ef-8398-d9ff35fc6bd6>: Beispiel Resource
+'''
+KOMPETENZ_BAUM_SUCHE = """
+SELECT distinct ?root_label ?level_1_label ?level_2_label ?level_3_label
+WHERE {
+  <https://lehrplan.yovisto.com/resource/lp/rp/c688300f-1c49-11ef-8398-d9ff35fc6bd6> <https://w3id.org/lehrplan/ontology/LP_00000008> ?level_1 .
+?level_1 rdfs:label ?level_1_label .
+  OPTIONAL {
+    ?level_1 <https://w3id.org/lehrplan/ontology/LP_00000008> ?level_2 .
+?level_2 rdfs:label ?level_2_label .
+    OPTIONAL {
+      ?level_2 <https://w3id.org/lehrplan/ontology/LP_00000008> ?level_3 .
+?level_3 rdfs:label ?level_3_label .
+    }
+  }
+BIND(<https://lehrplan.yovisto.com/resource/lp/rp/c688300f-1c49-11ef-8398-d9ff35fc6bd6> as ?root) .
+?root rdfs:label ?root_label
+}
+ORDER BY ?root ?level_1 ?level_2 ?level_3
+"""
+
